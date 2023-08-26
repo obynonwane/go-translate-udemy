90d4a28b5fe3a7f169dc86bd653d56f4ff175a9f
obynonwane <biostechnologyng@gmail.com>
2023-08-26 16:54:27 +0100

    Update

--- cli/cli.go
+++ cli/cli.go
@@ -1,7 +1,38 @@
+// Package cli
 package cli
 
+import "net/http"
+
+// Custom struct for body from cli input
 type RequestBody struct {
 	SourceLang string
 	TargetLang string
 	SourceText string
+}
+
+// google translate url
+const translateUrl = "https://translate.googleapis.com/translate_a/single"
+
+// RequestTranslate for making request to google api to translate a text to a gicen languate
+func RequestTranslate(body *RequestBody) {
+
+	// create http client
+	client := &http.Client{}
+
+	//initialize your request
+	req, err := http.NewRequest("GET", translateUrl, nil)
+
+	//extract your query params and create your query params for the request
+	query := req.URL.Query()
+	query.Add("client", "gtx")
+	query.Add("sl", body.SourceLang)
+	query.Add("tl", body.TargetLang)
+	query.Add("dt", "t")
+	query.Add("q", body.SourceText)
+
+	//encode your query params back to json
+	req.URL.RawQuery = query.Encode()
+
+	//make request to translate url
+	client.Do(req)
 }
--- main.go
+++ main.go
@@ -1,35 +1,68 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
+	"flag" //parsing cli arguements
+	"fmt"  //handling output
+	"os"   //handle cli operations
+	"strings"
+	"sync" //
 
-	"github.com/obynonwane/go-translate-udemy/cli"
+	"github.com/obynonwane/go-translate-udemy/cli" //custom package
 )
 
+// define a wait group to have a list of goroutine processes
+// so main function would not end before the processes
+var wg = sync.WaitGroup{}
+
+// declaration of global variables - to hold values entered via the cli
 var sourceLang string
 var targetLang string
 var sourceText string
 
-func int() {
+/*
+special function in golang that get executed before the main function
+this function was used to define cli flags (-s, -t, -st respectively)
+this flags allows user to specify source, target language and the text to translate
+*/
+func init() {
 	flag.StringVar(&sourceLang, "s", "en", "Source language[en]")
 	flag.StringVar(&targetLang, "t", "fr", "Target language[fr]")
 	flag.StringVar(&sourceText, "st", "", "Text to translate")
 }
 
+// Main function entry point for our golang cli application
 func main() {
+	//processes the cli args and assign their value to the global variable
 	flag.Parse()
+
+	//counts number of flags that where set if none it prints the options and exits the cli
 	if flag.NFlag() == 0 {
 		fmt.Println("Options")
 		os.Exit(1)
 	}
 
+	//define a channel - for comunication between goroutine and your main process
+	// telling th emain process when the goroutine process is finished
+	strChan := make(chan string)
+
+	//add goroutine to a waitgroup
+	wg.Add(1)
+
+	//return a pointer to a struct RequestBody in cli package from main package in main function
 	reqBody := &cli.RequestBody{
 		SourceLang: sourceLang,
 		TargetLang: targetLang,
 		SourceText: sourceText,
 	}
 
-	cli.RequestTranslate(reqBody)
+	//make a call to RequestTranslate function in cli package - making it a goroutine
+	go cli.RequestTranslate(reqBody, strChan, &wg)
+
+	//print goroutine response to channel for main process to acknoledge
+	processedStr := strings.ReplaceAll(<-strChan, "+", " ")
+	fmt.Printf("%s\n", processedStr)
+	close(strChan)
+
+	//wait for the goroutine to finish
+	wg.Wait()
 }
